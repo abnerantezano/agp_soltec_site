ba3bd4bad872b498887d2464aabe1e072234ceb1
Yahaira Harumi Chavez Vasquez <130882001+Katlesty@users.noreply.github.com>
2024-11-22 21:49:48 -0500

    update

--- site/scripts/menu.js
+++ site/scripts/menu.js
@@ -2,25 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuBoton = document.querySelector(".menu-boton");
     const menuDeslizable = document.querySelector(".menu-deslizable");
     const cerrarMenu = document.querySelector(".cerrar-menu");
-    const enlaces = document.querySelectorAll(".menu-deslizable a"); 
+    const enlaces = document.querySelectorAll(".menu-deslizable a");
 
-
+    // Mostrar el menú al presionar el botón
     menuBoton.addEventListener("click", () => {
         menuDeslizable.classList.add("active");
     });
 
-
+    // Ocultar el menú al presionar el botón de cierre
     cerrarMenu.addEventListener("click", () => {
         menuDeslizable.classList.remove("active");
     });
 
+    // Ocultar el menú al seleccionar un enlace
     enlaces.forEach((enlace) => {
         enlace.addEventListener("click", () => {
-
-            enlaces.forEach((link) => link.classList.remove("active"));
-            
-            enlace.classList.add("active");
-
             menuDeslizable.classList.remove("active");
         });
     });
